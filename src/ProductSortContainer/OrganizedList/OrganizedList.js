af470e053a6ba181bfc80dcc4e5ae9a9e5028e23
Cybre <cybre3@gmail.com>
2022-05-16 18:41:53 -0500

    added print and download buttons

--- src/ProductSortContainer/OrganizedList/OrganizedList.js
+++ src/ProductSortContainer/OrganizedList/OrganizedList.js
@@ -1,32 +1,53 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./OrganizedList.css";
+import html2pdf from 'html2pdf.js'
 
 function OrganizedList(props) {
   const { sortedList, clicked } = props;
   const sortedListArr = Array.from(sortedList);
-  console.log(sortedList)
-  useEffect(() => {
-    console.log("re-rendered");
-  });
+
+  function printList() {
+    var divContents = document.getElementById("ol-print-download").innerHTML;
+    var a = window.open("", "", "height=500, width=500");
+    a.document.write(divContents);
+    a.print();
+  }
+
+  function download() {
+    var element = document.getElementById("ol-print-download");
+    html2pdf(element);
+  }
 
   return (
     <div className="final-list-container">
-      <ul className="ol-start-list">
+      <div className="ol-btn-container">
+        <button
+          className="ol-print-list-btn"
+          onClick={() => {
+            printList();
+          }}
+        >
+          Print
+        </button>
+        <button id="downloadPDF" onClick={download}>Download</button>
+      </div>
+      <ul className="ol-start-list" id="ol-print-download">
         {clicked
           ? sortedListArr.map((letterGroup, index) => (
-              <div key={index} id="ol">
+              <div key={index} id="ol" className="ol-group">
                 <span>{letterGroup[0]}</span>
                 {letterGroup[1].map((item, index) => (
-                  <div key={index}>
-                    <span>
-                      {item.productName}:{item.productQty}
-                    </span>
+                  <div className="ol-item" key={index}>
+                    <span className="ol-prod-name">{item.productName}</span>
+                    <span className="ol-item-colon">:</span>
+                    <span className="ol-prod-qty">{item.productQty}</span>
                   </div>
                 ))}
               </div>
             ))
           : null}
       </ul>
+      <div id="editor"></div>
     </div>
   );
 }
